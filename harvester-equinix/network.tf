ca0eff77e758ea90d99c929675bc932d5556dd91
brooksphilip <brooksphilip@mac.com>
2023-03-07 14:21:11 -0500

    adding branch

--- harvester-equinix/network.tf
+++ harvester-equinix/network.tf
@@ -11,15 +11,15 @@
 #   private_ipv4_subnet_size = 4
 # }
 
-# resource "equinix_metal_vlan" "harvester" {
-#   description = "Harvester-VLAN"
-#   metro       = var.metro
-#   project_id  = equinix_metal_project.project.id
-#   vxlan       = 2
-# }
+resource "equinix_metal_vlan" "harvester" {
+  description = "Harvester-VLAN"
+  metro       = var.metro
+  project_id  = equinix_metal_project.project.id
+  vxlan       = 2
+}
 
-# resource "equinix_metal_gateway" "harvester" {
-#   project_id               = equinix_metal_project.project.id
-#   vlan_id                  = equinix_metal_vlan.harvester.id
-#   private_ipv4_subnet_size = 4
-# }
+resource "equinix_metal_gateway" "harvester" {
+  project_id               = equinix_metal_project.project.id
+  vlan_id                  = equinix_metal_vlan.harvester.id
+  private_ipv4_subnet_size = 4
+}
